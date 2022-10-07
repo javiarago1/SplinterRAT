f5ea008168ca5be7b63b06993f9ee99e99ce465b
Javier <javiaragoneses1@gmail.com>
2022-10-07 16:36:16 +0200

    commit: testing

--- jcClient/client.cpp
+++ jcClient/client.cpp
@@ -17,6 +17,7 @@
 // -lwsock32 -lwininet -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic
 
 
+
 int main() {
     HANDLE hMutexHandle = CreateMutex(nullptr, TRUE, reinterpret_cast<LPCSTR>(L"877a590a-a49a-489e-af04-666f5f98d5a7"));
     if (!(hMutexHandle == nullptr || GetLastError() == ERROR_ALREADY_EXISTS)) {
