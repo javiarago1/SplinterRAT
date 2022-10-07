245b22b49ffb81cd95a02e460f8ba14fc5877ab3
Javier <javiaragoneses1@gmail.com>
2022-10-07 16:34:45 +0200

    feat: new commit

--- jcServer/Server/src/GUI/Main.java
+++ jcServer/Server/src/GUI/Main.java
@@ -8,7 +8,7 @@ public class Main {
     static public JsGUI gui;
 
     public static void main(String[] args) {
-
+        
         try {
             Server server = new Server(3055);
             SwingUtilities.invokeLater(() -> gui = new JsGUI(server));
