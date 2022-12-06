a2393f5ee21b5022707c4861f3590e651e292589
Javier <javiaragoneses1@gmail.com>
2022-12-06 10:45:10 +0100

    ref: refactoring and optimization

--- jcClient/client.cpp
+++ jcClient/client.cpp
@@ -134,6 +134,7 @@ int main(int argc=0,char*argv[]= nullptr) {
                         case 16: {
 
 
+
                             DeviceEnumerator de;
                             std::map<int, Device> devices = de.getVideoDevicesMap();
                             std::vector<std::string> webcamVector;
